017fa5b57e5312c0895c9980a092debd391f48dc
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-12-28 16:50:29 +0530

    started with express

---
+++ Lecture 9- Introduction to Express/app.js
@@ -0,0 +1,22 @@
+//External Module
+const express = require('express')
+
+//Local Module
+const userRequestHandler = require('./user')
+
+const app = express()
+
+app.use((req, res, next) => {
+  console.log("Came in first middleware",req.url, req.method);
+  next();
+})
+
+app.use((req, res, next) => {
+  console.log("Came in second middleware",req.url, req.method);
+  res.send("<p>Welcome to my world</p>")
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+})
