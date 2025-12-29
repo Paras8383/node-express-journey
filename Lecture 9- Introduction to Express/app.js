dc465c6cf3817b838cef056060c7be80404944d9
Paras8383 <167594303+Paras8383@users.noreply.github.com>
2025-12-29 12:23:59 +0530

    completed lecture 9

--- Lecture 9- Introduction to Express/app.js
+++ Lecture 9- Introduction to Express/app.js
@@ -6,14 +6,19 @@ const userRequestHandler = require('./user')
 
 const app = express()
 
-app.use((req, res, next) => {
+app.use("/",(req, res, next) => {
   console.log("Came in first middleware",req.url, req.method);
   next();
 })
 
-app.use((req, res, next) => {
+app.post("/submit-details",(req, res, next) => {
   console.log("Came in second middleware",req.url, req.method);
   res.send("<p>Welcome to my world</p>")
+})
+
+app.use("/",(req, res, next) => {
+  console.log("Came in first middleware",req.url, req.method);
+  res.send("<p>Welcome to new world</p>")
 })
 
 const PORT = 3000;
